93a1c744d6cb0d41a127c6ff1158e0a69d3e3afd
artur696969 <klys4221@icloud.com>
2023-05-02 18:55:06 +0100

    Initial commit

---
+++ lesson1/lessons01.js
@@ -0,0 +1,39 @@
+let hello = 'hello'
+console.log(hello)
+let owu = "owu"
+console. log(owu)
+let com = "com"
+console.log(com)
+let ua = 'ua'
+     console.log(ua)
+let numb = 1
+console.log (numb)
+let b = 10
+console.log (b)
+let c = -999
+console.log (c)
+let d = 123
+console.log(d)
+let e = 3.14
+console.log(e)
+let f = 2.7
+console.log(f)
+let g = 16
+console.log(g)
+let true1 = "true"
+
+console. log (true1)
+let check2 = "false"
+console. log (check2)
+
+const firstname = 'Artur'
+const middlename = "Andriyovych"
+const lastname = 'Klus'
+const string1 = firstname + " " +middlename + " " +lastname;
+console.log(string1)
+let y = 100;
+let z = '100';
+let x = true;
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof x)
